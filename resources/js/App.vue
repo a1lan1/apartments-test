1758659e4592bd34da49bc9c257062fa1c345b62
a1lan1 <genesboard@gmail.com>
2023-09-16 14:41:52 +0300

    added BaseHeader.vue component with dark mode feature

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,19 @@
 <template>
-    <div class="common-layout"></div>
+    <div class="common-layout">
+        <BaseHeader />
+
+        <el-container>
+            <el-main class="layout-container"></el-main>
+        </el-container>
+    </div>
 </template>
+
+<script lang="ts" setup>
+    import BaseHeader from '@/components/layouts/BaseHeader.vue'
+</script>
+
+<style scoped>
+    .layout-container {
+        height: 500px;
+    }
+</style>
---
+++ resources/js/components/layouts/BaseHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-header>
+        <el-row justify="space-between">
+            <el-text class="logo" type="primary" size="large">
+                Apartments
+            </el-text>
+
+            <el-switch
+                :model-value="!isDark"
+                inline-prompt
+                size="large"
+                :active-icon="Sunny"
+                :inactive-icon="Moon"
+                @change="toggleDark()"
+            />
+        </el-row>
+    </el-header>
+</template>
+
+<script lang="ts" setup>
+    import { Moon, Sunny } from '@element-plus/icons-vue'
+    import { isDark, toggleDark } from '@/composables'
+</script>
+
+<style scoped>
+    .logo {
+        font-size: 25px;
+        font-weight: bold;
+    }
+</style>
