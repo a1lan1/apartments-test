752ff2373a6bac59b1f766fddc2402fb61cdebf8
a1lan1 <genesboard@gmail.com>
2023-09-16 13:24:57 +0300

    added filter scope for Apartment model

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -32,5 +33,32 @@ class Apartment extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @param Builder $query
+     * @param array|null $filter
+     */
+    public function scopeFilter(Builder $query, ?array $filter): void
+    {
+        $query->when(isset($filter['price']), function ($q) use ($filter) {
+            return $q->where('price', '>=', $filter['price'][0])
+                ->where('price', '<=', $filter['price'][1]);
+        })
+            ->when(isset($filter['name']), function ($q) use ($filter) {
+                return $q->where('name', 'LIKE', "%{$filter['name']}%");
+            })
+            ->when(isset($filter['bedrooms']), function ($q) use ($filter) {
+                return $q->where('bedrooms', $filter['bedrooms']);
+            })
+            ->when(isset($filter['bathrooms']), function ($q) use ($filter) {
+                return $q->where('bathrooms', $filter['bathrooms']);
+            })
+            ->when(isset($filter['storeys']), function ($q) use ($filter) {
+                return $q->where('storeys', $filter['storeys']);
+            })
+            ->when(isset($filter['garages']), function ($q) use ($filter) {
+                return $q->where('garages', $filter['garages']);
+            });
     }
 }
