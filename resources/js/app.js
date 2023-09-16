bb93f9ef7902141bee2f8a568afb7f3cb584e5c2
a1lan1 <genesboard@gmail.com>
2023-09-16 14:13:00 +0300

    added vue, vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import axios from 'axios'
+import App from './App.vue'
+import { createApp } from 'vue'
+
+window.axios = axios
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+const app = createApp(App)
+
+app.mount('#app')
