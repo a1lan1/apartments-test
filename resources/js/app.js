bb9302a81b71bcc85e7ee63a66fc608cd29fa5b1
a1lan1 <genesboard@gmail.com>
2023-09-16 14:22:38 +0300

    added element-plus ui framework

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 import App from './App.vue'
 import { createApp } from 'vue'
+// import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 const app = createApp(App)
 
+// app.use(ElementPlus)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
-    ],
-});
+        vue(),
+
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+    ]
+})
